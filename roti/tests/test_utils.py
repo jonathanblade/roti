33d1fe80563de5cd8ae3979d929bee99b6dd6150
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-03-30 13:28:12 +0300

    chore: rename

--- roti/tests/test_utils.py
+++ roti/tests/test_utils.py
@@ -11,11 +11,14 @@ class TestUtils(TestCase):
         date, lats, rows = read_roti("/content/roti/data/roti0010.10f")
         self.assertEqual(date, datetime(2010, 1, 1))
         self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
+        self.assertEqual(rows.shape, (20, 180))
 
         date, lats, rows = read_roti("/content/roti/data/roti1970.13f")
         self.assertEqual(date, datetime(2013, 7, 16))
         self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
+        self.assertEqual(rows.shape, (20, 180))
 
         date, lats, rows = read_roti("/content/roti/data/roti1920.20f")
         self.assertEqual(date, datetime(2020, 7, 10))
         self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
+        self.assertEqual(rows.shape, (20, 180))
