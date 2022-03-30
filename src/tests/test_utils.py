b7b68fd7b62a1d69aa8ac749101ebda575bc39f7
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-03-30 13:22:57 +0300

    tests: read_roti

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -11,3 +11,11 @@ class TestUtils(TestCase):
         date, lats, rows = read_roti("/content/roti/data/roti0010.10f")
         self.assertEqual(date, datetime(2010, 1, 1))
         self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
+
+        date, lats, rows = read_roti("/content/roti/data/roti1970.13f")
+        self.assertEqual(date, datetime(2013, 7, 16))
+        self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
+
+        date, lats, rows = read_roti("/content/roti/data/roti1920.20f")
+        self.assertEqual(date, datetime(2020, 7, 10))
+        self.assertEqual(lats.tolist(), list(range(89, 50, -2)))
