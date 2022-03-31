a54bdb84a7dad0b854811fe37e10da4abf4aeff9
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-03-31 22:45:44 +0300

    fix: tests

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -25,9 +25,8 @@ class TestUtils(TestCase):
 
     def test_load_data(self):
         data = load_data(datetime(2010, 1, 1), datetime(2010, 1, 3))
-        keys = data.keys()
-        self.assertEqual(len(keys), 2)
-        self.assertEqual(list(keys), [datetime(2010, 1, 1), datetime(2010, 1, 2)])
+        self.assertEqual(data.shape, (2, 20, 180))
+        self.assertEqual(list(data.index), [datetime(2010, 1, 1), datetime(2010, 1, 2)])
 
     def test_get_date_from_filename(self):
         date = get_date_from_filename("roti0010.10f")
