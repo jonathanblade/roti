e74c59d93b263604857cafc036b3aa71493b5284
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-04-01 16:23:21 +0300

    feat: add plot_roti

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from datetime import datetime, timedelta
 
@@ -48,3 +49,37 @@ def get_date_from_filename(fname):
     doy = int(fname[4:7])
     year = 2000 + int(fname[9:11])
     return datetime(year, 1, 1) + timedelta(days=doy - 1)
+
+def plot_roti(data):
+    plt.rcParams["font.size"] = 16
+    plt.rcParams["figure.figsize"] = (8, 8)
+    plt.rcParams["figure.dpi"] = 300
+    plt.rcParams["savefig.bbox"] = "tight"
+
+    MLON = np.linspace(0, 360, 180)
+    MLAT = np.linspace(90, 50, 20)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="polar")
+
+    ax.set_title(self.DATE.strftime("%d %B %Y"))
+    ax.set_theta_zero_location("S")
+    ax.set_xticklabels(["00 MLT", "", "06", "", "12", "", "18", ""])
+    ax.set_rgrids([90, 80, 70, 60, 50], angle=135)
+    ax.set_rlim(bottom=90, top=50)
+
+    theta, r = np.meshgrid(MLON, MLAT)
+    cax = ax.contourf(
+        np.deg2rad(theta),
+        r,
+        data,
+        levels=np.linspace(0, 1, 51),
+        cmap="jet",
+        vmin=0,
+        vmax=1,
+        extend="both",
+    )
+    cbar = fig.colorbar(
+        cax, ticks=np.linspace(0, 1, 6), orientation="horizontal", pad=0.1
+    )
+    cbar.set_label("ROTI, TECU/min")
