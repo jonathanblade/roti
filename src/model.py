bffa3f386be2a930b9c4e5ae1c051d054c5ece16
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-04-01 15:23:45 +0300

    refactor: model

--- src/model.py
+++ src/model.py
@@ -13,92 +13,72 @@ from tensorflow.keras.layers import (
     ZeroPadding2D,
 )
 
-class LeiLiuAE(Model):
-    def __init__(self, input_shape=(3, 20, 180, 1)):
-        super(LeiLiuAE, self).__init__()
-        self.encoder = self._build_encoder(input_shape)
-        encoder_shape = self.encoder.layers[-1].output_shape
-        self.decoder = self._build_decoder(encoder_shape)
+CONFIG = {
+    "encoder": [
+        ["conv", 16, (3, 3), (1, 1), (1, 1)],
+        ["convLSTM", 32, (5, 5), (1, 1), (2, 2)],
+        ["conv", 32, (3, 3), (2, 2), (1, 1)],
+        ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
+        ["conv", 64, (3, 3), (2, 2), (1, 1)],
+        ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
+    ],
+    "decoder": [
+        ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
+        ["deconv", 64, (3, 3), (2, 2), (1, 1)],
+        ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
+        ["deconv", 64, (4, 3), (2, 2), (1, 1)],
+        ["convLSTM", 32, (5, 5), (1, 1), (2, 2)],
+        ["conv", 16, (4, 3), (1, 1), (1, 1)],
+        ["conv", 1, (1, 1), (1, 1), (0, 0)],
+    ],
+}
 
-    def call(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+def add_layer(model, layer, use_normalization=False, use_activation=False):
+    layer_name = layer[0]
+    filters = layer[1]
+    kernel_size = layer[2]
+    strides = layer[3]
+    padding = layer[4]
 
-    def train_step(self, data):
-        x, y = data
-        with GradientTape() as tape:
-            middle_map = x[1:2,:,:,:]
-            pred_map = self(middle_map, training=True)
-            # Compute loss
-            loss = mean_absolute_error(y, pred_map)
-        # Compute gradients
-        gradients = tape.gradient(loss, self.trainable_variables)
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        # Update metrics
-        self.compiled_metrics.update_state(y, pred_map)
-        return {m.name: m.result() for m in self.metrics}
+    if (layer_name == "conv"):
+        # model.add(TimeDistributed(ZeroPadding2D(padding=padding)))
+        model.add(TimeDistributed(Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")))
+    if (layer_name == "convLSTM"):
+        # model.add(TimeDistributed(ZeroPadding2D(padding=padding)))
+        model.add(ConvLSTM2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same", return_sequences=True))
+    if (layer_name == "deconv"):
+        # model.add(TimeDistributed(ZeroPadding2D(padding=padding)))
+        model.add(TimeDistributed(Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")))
 
-    @property
-    def config(self):
-        return {
-            "encoder": [
-                ["conv", 16, (3, 3), (1, 1), (1, 1)],
-                ["convLSTM", 32, (5, 5), (1, 1), (2, 2)],
-                ["conv", 32, (3, 3), (2, 2), (1, 1)],
-                ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
-                ["conv", 64, (3, 3), (2, 2), (1, 1)],
-                ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
-            ],
-            "decoder": [
-                ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
-                ["deconv", 64, (3, 3), (2, 2), (1, 1)],
-                ["convLSTM", 64, (5, 5), (1, 1), (2, 2)],
-                ["deconv", 64, (4, 3), (2, 2), (1, 1)],
-                ["convLSTM", 32, (5, 5), (1, 1), (2, 2)],
-                ["conv", 16, (4, 3), (1, 1), (1, 1)],
-                ["conv", 1, (1, 1), (1, 1), (0, 0)],
-            ],
-        }
+    if use_normalization:
+        model.add(BatchNormalization())
 
-    def _add_layer(self, model, layer, use_normalization=False, use_activation=False):
-        layer_name = layer[0]
-        filters = layer[1]
-        kernel_size = layer[2]
-        strides = layer[3]
-        padding = layer[4]
+    if use_activation:
+        model.add(LeakyReLU())
 
-        if (layer_name == "conv"):
-            # model.add(ZeroPadding2D(padding=padding))
-            model.add(TimeDistributed(Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")))
-        if (layer_name == "convLSTM"):
-            # model.add(ZeroPadding2D(padding=padding))
-            model.add(ConvLSTM2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same", return_sequences=True))
-        if (layer_name == "deconv"):
-            # model.add(ZeroPadding2D(padding=padding))
-            model.add(TimeDistributed(Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")))
+    return model
 
-        if use_normalization:
-            model.add(BatchNormalization())
+def build_model(input_shape=(3, 20, 180, 1)):
+    model = Sequential(name="Lei Liu AE")
+    model.add(Input(shape=input_shape))
+    for layer in CONFIG.get("encoder"):
+        model = add_layer(model, layer)
+    for layer in CONFIG.get("decoder"):
+        model = add_layer(model, layer)
+    return model
 
-        if use_activation:
-            model.add(LeakyReLU())
+def update_weights(model, x, y):
+    with GradientTape() as tape:
+        pred_y = model(x)
+        # Compute loss
+        loss = mean_absolute_error(y, pred_y)
+    # Compute gradients
+    gradients = tape.gradient(loss, model.trainable_variables)
+    # Update weights
+    model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-        return model
-
-    def _build_encoder(self, input_shape):
-        encoder = Sequential(name="Encoder")
-        encoder.add(Input(shape=input_shape))
-        for layer in self.config.get("encoder"):
-            encoder = self._add_layer(encoder, layer)
-        # encoder.summary()
-        return encoder
-
-    def _build_decoder(self, input_shape):
-        decoder = Sequential(name="Decoder")
-        decoder.add(Input(shape=input_shape[1:]))
-        for layer in self.config.get("decoder"):
-            decoder = self._add_layer(decoder, layer)
-        # decoder.summary()
-        return decoder
+def train_loop(model, train_generator, epochs):
+    for epoch in range(1, epochs + 1):
+        for train_x, train_y in train_generator:
+            update_weights(model, train_x, train_y)
+    return model
