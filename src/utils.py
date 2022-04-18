e9f72967467400896cf379c1c4018dffed421d54
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-04-18 14:57:38 +0300

    fix: add leading zeros

--- src/utils.py
+++ src/utils.py
@@ -35,7 +35,7 @@ def read_roti(fpath):
 def read_gfz():
     df = pd.read_csv(CONTENT_DIR + "GFZ.csv",
                      parse_dates={"date": ["YYYY", "MM", "DD"]},
-                     date_parser=lambda y, m, d: datetime.strptime(y + m + d, "%Y%m%d")
+                     date_parser=lambda y, m, d: datetime.strptime(y + m.zfill(2) + d.zfill(2), "%Y%m%d")
                      )
     df = df.set_index("date")
     return df
